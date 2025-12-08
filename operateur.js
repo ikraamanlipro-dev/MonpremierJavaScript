0462f5a47b0a43d4be1d45962c669f916d5b5846
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2025-12-08 16:32:43 +0300

    méthode d'addition

---
+++ operateur.js
@@ -0,0 +1,15 @@
+// Ici je viens coder les opérateurs
+
+console.log("+");//Addittion
+console.log("-");//Soustraction
+console.log("*");//Multiplication
+console.log("/");//Division
+
+//Addittion
+let prixUnitaire = 3;
+let valeurAjouter = 7;
+let prixAdditionel1 = 3 + 7;//Première méthode d'addition
+let prixAdditionel2 = prixUnitaire + valeurAjouter;// Deuxième méthode d'addition 
+
+console.log("prixAdditionnel1: égale",prixAdditionel1);
+console.log("prixAdditionnel12: égale",prixAdditionel2);
