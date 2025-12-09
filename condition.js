fa2d35fcc2ec19bba6b5d1d6cdecf0ae262e726b
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2025-12-09 14:41:25 +0300

    création d'un fichier condition pour utiliser des condition

---
+++ condition.js
@@ -0,0 +1,9 @@
+let nombreFruit = 18 ;
+
+// Déclaration d'une condition
+// J'affiche l'alerte SI le nombre de fruit est égale à 18
+if(nombreFruit == 18) {  
+    alert("Vous avez atteint 18 fruits."); // Fontion alert
+}; 
+
+console.log(nombreFruit)
