bac53693fa30d60410174f4112c0d07b11d10acd
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2025-12-11 09:03:13 +0300

    calcul et mise en forme en css

---
+++ calcul_age.js
@@ -0,0 +1,13 @@
+let anneeDeNaissance = 2000;
+let annee = 2025;
+
+let ageCalcul = annee - anneeDeNaissance;
+
+console.log(ageCalcul);
+
+let anneeDeNaissance2 = 2000;
+let annee2 = 2025;
+
+let ageCalculV2 = annee2 - anneeDeNaissance2;
+
+console.log(ageCalculV2);
